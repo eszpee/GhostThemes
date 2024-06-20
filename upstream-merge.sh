702643929812f7fe52306b7096a57dfd325bfc3e
Péter Szász <eszpee@gmail.com>
2024-06-20 14:51:46 +0200

    doc clarifications

--- upstream-merge.sh
+++ upstream-merge.sh
@@ -1,8 +1,10 @@
-#!/bin/zsh
+####THIS SCRIPT IS NOT INTENDED TO RUN AUTOMATICALLY, DO THESE STEPS MANUALLY PLEASE #!/bin/zsh
 #git checkout -b TryGhost-main main  #Tryghost-main created from main, switched to that
-git checkout 959c06cc400f4e86c5c1086f25f171a80dc05b17 #go back in time, this was a common root before my changes
-git switch -c clean-main #create new branch from the root point in time
-#continue from here next time (but do a git checkout clean-main before)
+#####WHEN DOING FIRST TIME
+#git checkout 959c06cc400f4e86c5c1086f25f171a80dc05b17 #go back in time, this was a common root before my changes
+#git switch -c clean-main #create new branch from the root point in time
+######WHEN DOING USUALLY (but do a git checkout clean-main before)
+git checkout clean-main
 git pull https://github.com/TryGhost/Themes.git main #pull upstream to this new branch
 git checkout main #let's go back home
 git merge --no-ff clean-main #merge the changes to ours
